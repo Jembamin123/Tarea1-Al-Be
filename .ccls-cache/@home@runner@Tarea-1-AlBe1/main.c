282a298c3280d0e74a691afb68bdec4daf946e8b
Jembamin123 <142074934+Jembamin123@users.noreply.github.com>
2023-08-29 23:28:05 +0000

    Empezaremos a trabajar :D

--- .ccls-cache/@home@runner@Tarea-1-AlBe1/main.c
+++ .ccls-cache/@home@runner@Tarea-1-AlBe1/main.c
@@ -3,37 +3,100 @@
 #include <string.h>
 #include "list.h"
 
-typedef struct{
-    int year;
-    char title[30];
-    char director[30];
-}Pelicula;
+// Estructuras
+typedef struct Estudiante{
+    char nombre[51];
+    char codigo[21];
+    struct Estudiante* siguiente;
+} Estudiante;
 
+typedef struct Libro{
+    char titulo[51];
+    char autor[51];
+    char genero[51];
+    char ISBN[21];
+    char ubicacion[51];
+    char estado[21];
+    Estudiante* reservas;
+    struct Libro* siguiente;
+} Libro;
 
-int main( int argc, char *argv[] ){
+// Funciones de implementacion (registrar,mostrar,reservar,etc.)
 
-    List* movies= createList();
-    Pelicula* p= (Pelicula*) malloc (sizeof(Pelicula));
-    p->year=2004;
-    strcpy(p->title,"2046");
-    strcpy(p->director,"Wong Kar Wai");
-    pushBack(movies,p);
+int main(){
+  Libro* biblioteca = NULL;
+  int opcion = -1; 
 
-    p= (Pelicula*) malloc (sizeof(Pelicula));
-    p->year=2016;
-    strcpy(p->title,"Arrival");
-    strcpy(p->director,"Denis Villeneuve ");
-    pushBack(movies,p);
-
-
-    Pelicula* aux = firstList(movies);
-    while(aux!=NULL){
-        printf("%s (%d)\n",aux->title,aux->year);
-        aux=nextList(movies);
+  while(opcion!=0){
+      printf("------------------------------------------");
+      printf("\n           Menú de libreria\n");
+      printf("------------------------------------------\n\n");
+      printf("1. Registrar libro\n");
+      printf("2. Mostrar datos de libro\n");
+      printf("3. Mostrar todos los libros\n");
+      printf("4. Reservar libro\n");
+      printf("5. Cancelar reserva de libro\n");
+      printf("6. Retirar libro\n");
+      printf("7. Devolver libro\n");
+      printf("8. Mostrar libros prestados\n");
+      printf("9. Importar libros desde un archivo CSV\n");
+      printf("10. Exportar libros a un archivo CSV\n");
+      printf("0. Salir\n\n");
+      printf("Selecciona una opción: ");
+      scanf("%i", &opcion);
+      
+      switch(opcion){
+          case 1:
+            // Registrar libro
+            // registrarLibro(&biblioteca, ...);
+            break;
+          case 2:
+            // Mostrar datos de libro
+            // mostrarDatosLibro(biblioteca);
+            break;
+        case 3:
+            // Mostrar todos los libros
+            // mostrarLibros(biblioteca);
+            break;
+        case 4:
+            // Reservar libro
+            // reservarLibro(biblioteca);
+            break;
+        case 5:
+            // Cancelar reserva de libro
+            // cancelarReservaLibro(biblioteca);
+            break;
+        case 6:
+            // Retirar libro
+            // retirarLibro(biblioteca);
+            break;
+        case 7:
+            // Devolver libro
+            // devolverLibro(biblioteca);
+            break;
+        case 8:
+            // Mostrar libros prestados
+            // mostrarLibrosPrestados(biblioteca);
+            break;
+        case 9:
+            // Importar libros desde un archivo CSV
+            // importarLibrosDesdeCSV(&biblioteca);
+            break;
+        case 10:
+            // Exportar libros a un archivo CSV
+            // exportarLibrosACSV(biblioteca);
+            break;
+        case 0:
+            printf("\n¡Hasta luego!\n");
+            break;
+        default:
+            printf("\nOpción inválida. Por favor, selecciona una opción válida.\n");
+            break;
+      }
     }
 
-    
+  // Liberar memoria de la biblioteca y las reservas
+  // liberarBiblioteca(biblioteca);
 
-    return 0;
+  return 0;
 }
-//sadsdsadsa
